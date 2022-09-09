342c2ccebde78cab76c6459e4fa0cabd224a1756
ChaabaneAnas <anas.chaabane98@gmail.com>
2022-09-09 11:01:31 +0100

    add Onclik event &lift State

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import { Display, Keyboard, Operations } from './Components/calculator';
+import calculate from './logic/calculate';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+    this.setState(calculate({}, 'AC'));
+  }
+
+  handleClick = (e) => {
+    this.setState((prevState) => calculate(prevState, e.target.textContent));
+  };
+
   render() {
     return (
       <div className="app-container">
-        <Display result={0} />
+        <Display state={this.state} />
         <div className="second-row">
-          <Keyboard />
-          <Operations />
+          <Keyboard handleClick={this.handleClick} />
+          <Operations handleClick={this.handleClick} />
         </div>
       </div>
     );
